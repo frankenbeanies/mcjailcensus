e02435d1ebc092f745d0708b1a4e086f7b306d06
frankenbeanies <me@samuelbostick.com>
2014-01-07 04:29:36 -0500

    Created Inmate Model

---
+++ mcjailcensus/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Inmate(models.Model):
+	last = models.CharField(max_length=100)
+	dob = models.DateTimeField()
+	sex = models.CharField(max_length=1)
+	middle = models.CharField(max_length=1)
+	race = models.CharField(max_length=1)
+	mcid = models.IntegerField(max_length=6)
+	first = models.CharField(max_length=100)
