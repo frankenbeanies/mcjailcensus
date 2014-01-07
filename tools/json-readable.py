fdea203bff0fb6eb44d5d2b79d2357c5111443dd
frankenbeanies <me@samuelbostick.com>
2014-01-07 03:53:03 -0500

    Added json => csv for inmates

---
+++ tools/json-readable.py
@@ -0,0 +1,43 @@
+#Parse json to csv with format last,dob,sex,middle,race,mcid,first
+import re
+
+f = open('../docs/census16.pdf.json')
+w = open('../docs/readable.csv', 'w')
+
+prev = ''
+
+for line in f:
+	line = re.findall(r'\"(.*?)\"', line)
+
+	i = 0
+
+	for item in line:
+		if 'last' in prev and item is not '':
+			w.write(item + ',')
+		if 'dob' in prev or 'sex' in prev or 'middle' in prev or 'race' in prev or 'mcid' in prev:
+			w.write(item + ',')
+		elif 'first' in prev:
+			w.write(item + '\n')
+
+		prev = item
+		i += 1
+
+f.close()
+w.close()
+
+f = open('../docs/readable.csv')
+
+lst = []
+
+for line in f:
+	if line[0] is not ',':
+		lst.append(line)
+
+f.close()
+
+f = open('../docs/readable.csv', 'w')
+
+for line in lst:
+	f.write(line)
+
+f.close()
